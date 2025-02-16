651a98217a5ad9a64a60f9eaff25fd0df338d322
DouceNouni <roxy.perruche@hotmail.com>
2025-02-15 21:00:40 -0500

    Extremely simple and sloppy program to test Slot serialization.
    Clicking the "Test!" button opens a file window. The chosen package's Slot resources will be read and rewritten into a new one, SlotSerializationTest.

---
+++ TestZone/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace TestZone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectPackageDialog = new System.Windows.Forms.OpenFileDialog();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // selectPackageDialog
+            // 
+            this.selectPackageDialog.FileName = "selectPackageDialog";
+            this.selectPackageDialog.Filter = "Sims 2 Package Files (*.package)|*.package";
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(228, 145);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(349, 134);
+            this.buttonTest.TabIndex = 0;
+            this.buttonTest.Text = "Test!";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog selectPackageDialog;
+        private System.Windows.Forms.Button buttonTest;
+    }
+}
+
---
+++ TestZone/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Sims2Tools;
+using Sims2Tools.DBPF;
+using Sims2Tools.DBPF.Data;
+using Sims2Tools.DBPF.CPF;
+using Sims2Tools.DBPF.Package;
+using Sims2Tools.DBPF.SceneGraph.BINX;
+using Sims2Tools.DBPF.SceneGraph.COLL;
+using Sims2Tools.DBPF.SceneGraph.GZPS;
+using Sims2Tools.DBPF.SceneGraph.IDR;
+using Sims2Tools.DBPF.SceneGraph.TXMT;
+using Sims2Tools.DBPF.SceneGraph.TXTR;
+using Sims2Tools.DBPF.SceneGraph.XTOL;
+using Sims2Tools.DBPF.STR;
+using Sims2Tools.DBPF.Utils;
+using Sims2Tools.DbpfCache;
+using System.Diagnostics;
+using Sims2Tools.DBPF.SceneGraph;
+using Microsoft.VisualBasic;
+using Sims2Tools.DBPF.SceneGraph.XMOL;
+using Sims2Tools.Updates;
+using Sims2Tools.DBPF.SLOT;
+
+namespace TestZone
+{
+    public partial class Form1 : Form
+    {
+        DBPFFile sourcePackage;
+        DBPFFile copiedPackage;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            if (selectPackageDialog.ShowDialog() == DialogResult.OK)
+            {
+                sourcePackage = new DBPFFile(selectPackageDialog.FileName);
+
+                string newPackageFilepath = string.Format("{0}/SlotSerializationTest.package", sourcePackage.PackageDir);
+                copiedPackage = new DBPFFile(newPackageFilepath);
+
+                List<DBPFEntry> slotEntries = sourcePackage.GetEntriesByType(Slot.TYPE);
+                foreach (DBPFEntry entry in slotEntries)
+                {
+                    Slot slotResource = (Slot)sourcePackage.GetResourceByEntry(entry);
+
+                    copiedPackage.Commit(slotResource, true);
+                }
+
+                copiedPackage.Update(false);
+
+                copiedPackage.Close();
+                sourcePackage.Close();
+            }
+        }
+    }
+}
